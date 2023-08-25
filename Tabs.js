a8e01147628b485c1f4b550509c68a9afd62ba64
VishalSharma00123 <vishal2k21ml@gmail.com>
2023-08-25 15:08:10 +0530

    first commit

--- Tabs.js
+++ Tabs.js
@@ -34,38 +34,80 @@ function Tabs() {
                 <div
                     className={toggleState === 1 ? 'content active-content' : 'content'}
                 >
-                    <h2>Content 1</h2>
-                    <hr />
-                    <p>
+                    <div className='left'>
+                        <h2>content 1</h2>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
                         labore cumque aperiam sequi laudantium odit illum tenetur porro
                         adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
                         molestiae maiores tempore voluptate excepturi possimus exercitationem?
-                    </p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                    </div>
+                    <div className='right'>
+                        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="My Image" />
+                    </div>
                 </div>
                 <div
                     className={toggleState === 2 ? 'content active-content' : 'content'}
                 >
-                    <h2>Content 2</h2>
-                    <hr />
-                    <p>
+                    <div className='left'>
+                        <h2>content 2</h2>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
                         labore cumque aperiam sequi laudantium odit illum tenetur porro
                         adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
                         molestiae maiores tempore voluptate excepturi possimus exercitationem?
-                    </p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                    </div>
+                    <div className='right'>
+                        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="My Image" />
+                    </div>
                 </div>
                 <div
                     className={toggleState === 3 ? 'content active-content' : 'content'}
                 >
-                    <h2>Content 3</h2>
-                    <hr />
-                    <p>
+                    <div className='left'>
+                        <h2>content 3</h2>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
                         labore cumque aperiam sequi laudantium odit illum tenetur porro
                         adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
                         molestiae maiores tempore voluptate excepturi possimus exercitationem?
-                    </p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+                        labore cumque aperiam sequi laudantium odit illum tenetur porro
+                        adipisci reprehenderit nihil minus, architecto omnis quasi quam vitae
+                        molestiae maiores tempore voluptate excepturi possimus exercitationem?
+                    </div>
+                    <div className='right'>
+                        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="My Image" />
+                    </div>
                 </div>
             </div>
         </div>
